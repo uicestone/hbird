29c215b038ec00e523de85ce6cc14d08c80f01e8
陆秋石 <uicestone@gmail.com>
2014-01-15 19:06:48 +0800

    将前端导入主题

---
+++ wp-content/themes/hbird/home.php
@@ -0,0 +1,293 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>蜂鸟——企业形象管理专家</title>
+	<link rel="stylesheet" type="text/css" href="http://cdn.staticfile.org/twitter-bootstrap/2.3.2/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="http://cdn.staticfile.org/twitter-bootstrap/2.3.2/css/bootstrap-responsive.min.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+</head>
+
+<body>
+	<div id="home">
+		<div class="home-bg"></div>
+		<div class="title">
+			<div class="intro-line"></div>
+			<h1>Hello</h1>
+			<h1 class="small">Welcome to HBird</h1>
+			<div class="intro-line"></div>
+			<p>
+				只做&emsp;抓人眼球的事&emsp;用视觉&emsp;思考&emsp;表达
+			</p>
+		</div>
+	</div>
+
+	<div class="navbar">
+		<div class="navbar-inner">
+			<ul class="nav">
+				<li class="divider-vertical"></li>
+				<li><a href="#home">HOME / 首页</a></li>
+				<li class="divider-vertical"></li>
+				<li><a href="#work">WORK / 案例</a></li>
+				<li class="divider-vertical"></li>
+				<li><a href="#product">PRODUCT / 产品</a></li>
+				<li class="divider-vertical"></li>
+				<li><a href="#about">US / 我们</a></li>
+				<li class="divider-vertical"></li>
+				<li><a href="#news">NEWS / 动态</a></li>
+				<li class="divider-vertical"></li>
+				<li><a href="#contact">CONTACT / 联系</a></li>
+				<li class="divider-vertical"></li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="bg" id="work">
+		<div class="container">
+			<h2 class="title-light">
+				<span>WORK<small>	我们的案例</small></span>
+			</h2>
+			<div class="row">
+				<div class="span6">
+					<p>
+						asldkfjskdalfjalsdkfjl,  asdjflkasdjflaskdfj,  , lkasdjflsadf,  jdlasfkjals, fkjalddjfls, dfjdlasfkjalsfkja, lddjflsadfjd, lasfkjalsfkja, lddjf, lsadfjdl, asfkja, lsfkjalddjflsadfjdlasfkj, alsfkjaldd, jflsadfjdlasf, kjalsfkjalddjflsadfjdlasfkjalsfkja, lddjflsadfjdlasfkjalsfkjalddjflsadfjdlasfkjalsfkjald, 
+					</p>
+				</div>
+				<div class="span6">
+					<p>
+						我们为我们的每一个案例自豪。对每一个项目，无论类型、大小，我们都倾注全部的精力；一路探索，一路成长；感谢伙伴们，知遇理解；感谢同路人，扶持相随；5年的心路历程，有阳光、有汗水，但总充满欢笑，我们收获满满。尊重、信赖、精彩、感动……
+					</p>
+				</div>
+			</div>
+			<br />
+			<ul class="nav nav-tabs">
+				<li class="active">
+					<a href="#a" data-toggle="tab">GRAPHIC / 平面</a>
+				</li>
+				<li><a href="#b" data-toggle="tab">MULTIMEDIA / 多媒体</a></li>
+				<li><a href="#c" data-toggle="tab">PUBLIC RELATIONS ACTIVITIES / 公关活动</a></li>
+			</ul>
+			<div class="tab-content">
+				<div class="tab-pane fade active in" id="a">
+					<div class="row">
+						<div class="span4">
+							<a href="#"><img src="#"></a>
+						</div>
+						<div class="span4">
+							<a href="#"><img src="#"></a>
+						</div>
+						<div class="span4">
+							<a href="#"><img src="#"></a>
+						</div>
+					</div>
+				</div>
+
+				<div class="tab-pane fade" id="b">
+					<div class="row">
+						<div class="span4">
+							<a href="#"><img src="#"></a>
+						</div>
+						<div class="span4">
+							<a href="#"><img src="#"></a>
+						</div>
+						<div class="span4">
+							<a href="#"><img src="#"></a>
+						</div>
+					</div>
+				</div>
+
+				<div class="tab-pane fade" id="c">
+					<div class="row">
+						<div class="span4">
+							<a href="#"><img src="#"></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Product Section -->
+	<div id="product-cover">
+		<div class="product-bg"></div>
+	</div>
+	<div class="bg" id="product">
+		<div class="container">
+			<h2 class="title-dark">
+				<span>PRODUCT<small>	我们的产品</small></span>
+			</h2>
+			<div class="row">
+				<div class="span6">
+					<p>
+						asldkfjskdalfjalsdkfjl,  asdjflkasdjflaskdfj,  , lkasdjflsadf,  jdlasfkjals, fkjalddjfls, dfjdlasfkjalsfkja, lddjflsadfjd, lasfkjalsfkja, lddjf, lsadfjdl, asfkja, lsfkjalddjflsadfjdlasfkj, alsfkjaldd, jflsadfjdlasf, kjalsfkjalddjflsadfjdlasfkjalsfkja, lddjflsadfjdlasfkjalsfkjalddjflsadfjdlasfkjalsfkjald, 
+					</p>
+				</div>
+				<div class="span6">
+					<p>
+						laksdjglasdkjglsadkjgelqw;kgjewq;gkjqwe;ogijewqog;jqero;bijeor;igjewqgijqwe[iotqwejtgwqejgkl;bndfa;leqng;qwejgoiqwegjweqoigjk]
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="span3">
+					<img src="">
+					<h5>广告策略与创意</h5>
+					<h5>标志与 VI 系统</h5>
+					<h5>平面设计印刷</h5>
+					<h5>衍生产品设计</h5>
+				</div>
+				<div class="span3">
+					<img src="">
+					<h5>广告策略与创意</h5>
+					<h5>标志与 VI 系统</h5>
+					<h5>平面设计印刷</h5>
+					<h5>衍生产品设计</h5>
+				</div>
+				<div class="span3">
+					<img src="">
+					<h5>广告策略与创意</h5>
+					<h5>标志与 VI 系统</h5>
+					<h5>平面设计印刷</h5>
+					<h5>衍生产品设计</h5>
+				</div>
+				<div class="span3">
+					<img src="">
+					<h5>广告策略与创意</h5>
+					<h5>标志与 VI 系统</h5>
+					<h5>平面设计印刷</h5>
+					<h5>衍生产品设计</h5>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- About Section -->
+	<div id="about-cover">
+		<div class="about-bg"></div>
+	</div>
+	<div class="bg" id="about">
+		<div class="container">
+			<h2 class="title-light">
+				<span>ABOUT<small>	我们是谁</small></span>
+			</h2>
+			<div class="row">
+				<div class="span6">
+					<p>
+						asldkfjskdalfjalsdkfjl,  asdjflkasdjflaskdfj,  , lkasdjflsadf,  jdlasfkjals, fkjalddjfls, dfjdlasfkjalsfkja, lddjflsadfjd, lasfkjalsfkja, lddjf, lsadfjdl, asfkja, lsfkjalddjflsadfjdlasfkj, alsfkjaldd, jflsadfjdlasf, kjalsfkjalddjflsadfjdlasfkjalsfkja, lddjflsadfjdlasfkjalsfkjalddjflsadfjdlasfkjalsfkjald, 
+					</p>
+				</div>
+				<div class="span6">
+					<p>
+						laksdjglasdkjglsadkjgelqw;kgjewq;gkjqwe;ogijewqog;jqero;bijeor;igjewqgijqwe[iotqwejtgwqejgkl;bndfa;leqng;qwejgoiqwegjweqoigjk]
+					</p>
+				</div>
+			</div>
+
+			<h4 class="about-small-title">
+				CONCEPT
+				<span class="about-small-title-zh">理念</span>
+			</h4>
+			<img src="">
+
+			<h4 class="about-small-title">
+				COMPANIONS
+				<span class="about-small-title-zh">同行者</span>
+			</h4>
+			<h4 class="about-small-content-title">我们有坚持</h4>
+			<p class="about-small-content">但我们更尊重客户 客户远比我们了解自己</p>
+			<h4 class="about-small-content-title">我们有策略</h4>
+			<p class="about-small-content">但我们更注重对话沟通 精准诠释品牌的动人价值</p>
+			<h4 class="about-small-content-title">我们有控制</h4>
+			<p class="about-small-content">严把成本质量关 令客户省心免除后顾之忧</p>
+
+			<div class="row">
+				<a class="span1" href="#"><img src=""></a>
+				<a class="span1" href="#"><img src=""></a>
+				<a class="span1" href="#"><img src=""></a>
+				<a class="span1" href="#"><img src=""></a>
+				<a class="span1" href="#"><img src=""></a>
+				<a class="span1" href="#"><img src=""></a>
+				<a class="span1" href="#"><img src=""></a>
+				<a class="span1" href="#"><img src=""></a>
+				<a class="span1" href="#"><img src=""></a>
+			</div>
+
+			<h4 class="about-small-title">
+				OUT TEAM
+				<span class="about-small-title-zh">我们的团队</span>
+			</h4>
+			<p class="about-small-content">团队组合 犹如小小的溪流汇聚成河</p>
+			<p class="about-small-content">每个项目的过程都分初始 进行 收尾</p>
+			<p class="about-small-content">蜂鸟团队始终如一的融入项目 成员见相互激发灵感 创意和工作激情</p>
+			<p class="about-small-content">坚持每个步骤 坚定每个环节 我们通力合作</p>
+			<p class="about-small-content">市场策略 / 创意设计 / 客户服务 / 制作服务</p>
+
+			<div class="row">
+				<a class="about-button" href="#">历届蜂鸟杯</a>
+				<a class="about-button" href="#">员工生日会</a>
+				<a class="about-button" href="#">快乐工作</a>
+				<a class="about-button-hot" href="#">五周年庆祝</a>
+			</div>
+		</div>
+	</div>
+
+	<!-- News Section -->
+	<div id="news-cover">
+		<div class="news-bg"></div>
+	</div>
+	<div class="bg" id="news">
+		<div class="container">
+			<h2 class="title-dark">
+				<span>NEWS<small>	我们的动态</small></span>
+			</h2>
+
+			<div class="row">
+				<a class="news-button" href="#">历届蜂鸟杯</a>
+				<a class="news-button" href="#">员工生日会</a>
+				<a class="news-button" href="#">快乐工作</a>
+				<a class="news-button" href="#">五周年庆祝</a>
+			</div>
+		</div>
+	</div>
+
+	<!-- Contact Section -->
+	<div id="contact-cover">
+		<div class="contact-bg"></div>
+	</div>
+	<div class="bg" id="contact">
+		<div class="container">
+			<h2 class="title-light">
+				<span>CONTACT<small>	联系我们</small></span>
+			</h2>
+			<div class="row" id="map" style="height: 300px;"></div>
+			<div class="row contacts">
+				<div class="span3 offset1">
+					<h3>CONTACT <span>联系我们</span></h3>
+				</div>
+				<div class="span6 offset5">
+					<p>上海晶利广告设计有限公司 Shanghai Jingli AD. Design Co., LTD</p>
+					<p><span>Add: </span>上海市瞿西路1236号（空间的表情）综合楼211/206室</p>
+					<p><span>Tel: </span>86-21 53083527</p>
+					<p><span>Fax: </span>86-21 53828941</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script type="text/javascript" src="http://cdn.staticfile.org/jquery/1.10.2/jquery.min.js"></script>
+	<script type="text/javascript" src="http://cdn.staticfile.org/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=fjvH5f1Gvj5xu24eapyfplh5"></script>
+
+	<script type="text/javascript">
+		var map = new BMap.Map("map");                        // 创建Map实例
+		map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);     // 初始化地图,设置中心点坐标和地图级别
+		map.addControl(new BMap.NavigationControl());               // 添加平移缩放控件
+		map.addControl(new BMap.ScaleControl());                    // 添加比例尺控件
+		map.addControl(new BMap.OverviewMapControl());              //添加缩略地图控件
+		map.enableScrollWheelZoom();                            //启用滚轮放大缩小
+		map.addControl(new BMap.MapTypeControl());          //添加地图类型控件
+		map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+	</script>
+</body>
+</html>
