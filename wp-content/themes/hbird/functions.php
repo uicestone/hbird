ac716fb0b60b3480c1758a7a52c4f72bae54d867
陆秋石 <uicestone@gmail.com>
2014-01-16 21:43:41 +0800

    自动载入样式资源

---
+++ wp-content/themes/hbird/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+add_action('wp_enqueue_scripts', function(){
+	wp_register_style('bootstrap', 'http://cdn.staticfile.org/twitter-bootstrap/2.3.2/css/bootstrap.min.css');
+	wp_register_style('bootstrap-responsive', 'http://cdn.staticfile.org/twitter-bootstrap/2.3.2/css/bootstrap-responsive.min.css', array('bootstrap'));
+	wp_enqueue_style('bootstrap-responsive');
+	wp_enqueue_style('style', get_stylesheet_uri());
+});
